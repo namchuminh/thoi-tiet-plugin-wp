7d85f0307e5e98f681e42cf2bd63be6bf58b1afe
namchuminh <41958727+namchuminh@users.noreply.github.com>
2022-11-04 00:41:04 +0700

    Update action_form_setting.php

--- includes/action_form_setting.php
+++ includes/action_form_setting.php
@@ -1,5 +1,11 @@
 <?php 
+if ( !function_exists( 'add_action' ) ) {
+	echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
+	exit;
+}
+
 require($_SERVER['DOCUMENT_ROOT'] . '/wp-load.php');//nhập file wp-load.php để sử dụng hàm update_option()
+
 if(isset($_POST['city'])){
 	if(empty($_POST['city'])){
 		$_POST['city'] = "Hanoi";
@@ -10,3 +16,6 @@ if(isset($_POST['city'])){
     update_option('nhiet_do',$_POST['temp'],'','yes');
 }
 
+if (wp_redirect(BASE_URL.'/wp-admin/options-general.php?page=thoi_tiet')) {
+    exit;
+}
