0fe6ceb444216acc15d39576ce941f88a7666930
namchuminh <41958727+namchuminh@users.noreply.github.com>
2022-11-04 00:32:56 +0700

    Update action_form_setting.php

--- includes/action_form_setting.php
+++ includes/action_form_setting.php
@@ -8,6 +8,5 @@ if(isset($_POST['city'])){
 	echo $_POST['temp'];
     update_option('thanh_pho',$_POST['city'],'','yes'); //cập nhật thông tin thành phố vào bảng wp_options
     update_option('nhiet_do',$_POST['temp'],'','yes');
-    wp_redirect(BASE_URL.'/wp-admin/options-general.php?page=thoi_tiet');
 }
 
