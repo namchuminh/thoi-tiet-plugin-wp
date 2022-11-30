837ff4e6de2721715067b44bbef4dbe849f6d80e
Chu Minh Nam <41958727+namchuminh@users.noreply.github.com>
2022-11-30 10:41:11 +0700

    Update action_form_setting.php

--- includes/action_form_setting.php
+++ includes/action_form_setting.php
@@ -5,8 +5,7 @@ if(isset($_POST['city'])){
 	if(empty($_POST['city'])){
 		$_POST['city'] = "Hanoi";
 	}
-	echo $_POST['city'];
-	echo $_POST['temp'];
     update_option('thanh_pho',$_POST['city'],'','yes'); //cập nhật thông tin thành phố vào bảng wp_options
     update_option('nhiet_do',$_POST['temp'],'','yes');
+    echo "Cập Nhật Thành Công!";
 }
